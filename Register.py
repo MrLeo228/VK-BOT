887e529db59dec1e8dbc4c41b8e68ba223e32a48
MrLeo228 <79750132+MrLeo228@users.noreply.github.com>
2021-02-27 16:06:59 +0300

    Delete Register.py

--- Register.py
+++
@@ -1,6 +0,0 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll
-
-token = '6c9fda8bb0392af480cb06eac528f830cd4e876521957fd9ad08e11d2e2b45a7caecc68f71982788a00a5'
-authorize = vk_api.VkApi(token=token)
-long_poll = VkLongPoll(authorize)
